43864d92df6d9dab242f3ea049a6788190f37298
Harry thuku <38860432+hthuku95@users.noreply.github.com>
2025-08-13 22:10:31 +0300

    Add src/services/mod.rs

---
+++ src/services/mod.rs
@@ -0,0 +1,11 @@
+pub mod auth;
+pub mod map;
+pub mod routing;
+pub mod search;
+pub mod user;
+
+pub use auth::AuthService;
+pub use map::MapService;
+pub use routing::RoutingService;
+pub use search::SearchService;
+pub use user::UserService;
